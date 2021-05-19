0b2c20373deb088320ccf43449406fb4ff4d4973
itsclarence <postclarence@gmail.com>
2021-05-19 13:24:26 +0200

    Update hooks.c

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -191,7 +191,7 @@ static int __init enforcing_setup(char *str)
 	  defined(SELINUX_DEFAULT_PERMISSIVE)
 		selinux_enforcing = 0;
 #else
-		selinux_enforcing = enforcing ? 1 : 0;
+		selinux_enforcing = 0;// enforcing ? 1 : 0;
 #endif
 	return 1;
 }
